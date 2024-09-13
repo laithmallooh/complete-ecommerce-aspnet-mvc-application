d7b006d74bbd56f6122202b6bf6bca94299874aa
Laith <laithmallooh@gmail.com>
2024-09-13 12:35:09 +0300

    create models and initialize the data base

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using eTickets.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Add DbContext configuration here
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 
 var app = builder.Build();
 
@@ -12,6 +19,13 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+//seed database
+AppDbInitializer.Seed(app);
+app.Run();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
