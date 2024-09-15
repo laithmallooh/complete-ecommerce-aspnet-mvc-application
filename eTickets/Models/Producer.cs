5329c5b54c885f6f7e295f2db948abdb388daab9
Laith <agileindustries2023@gmail.com>
2024-09-15 17:03:45 +0300

    work on views

--- eTickets/Models/Producer.cs
+++ eTickets/Models/Producer.cs
@@ -10,8 +10,14 @@ namespace eTickets.Models
     {
         [Key]
         public int Id { get; set; }
+        [Display(Name ="Profile Picture")]
         public string ProfilePictureURL { get; set; }
+
+        [Display(Name = "Full Name")]
+
         public string FullName { get; set; }
+
+        [Display(Name = "Biograpgy")]
         public string Bio { get; set; }
 
         // Relationships
--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -16,17 +16,11 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // The default HSTS value is 30 days. You may want to change this for production scenarios.
     app.UseHsts();
 }
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-//seed database
-AppDbInitializer.Seed(app);
-app.Run();
-
+// Use middleware in the correct order
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
@@ -34,8 +28,13 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+// Define routing only once
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+// Seed database
+AppDbInitializer.Seed(app);
+
+// Ensure app.Run is at the end of the middleware pipeline
 app.Run();
