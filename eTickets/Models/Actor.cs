420d4505bd40ddfc7a86b6c762851565a0151015
Laith <laithmallooh@gmail.com>
2024-09-20 11:44:18 +0300

    implement all (add , deletel , update , show ) functionalities with the views
    and NotFound component

--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -12,17 +13,22 @@ namespace eTickets.Models
         [Key]
         public int Id { get; set; }
 
-        [Display(Name ="Profile Picture URL")]
+        [Display(Name = "Profile Picture")]
+        [Required(ErrorMessage = "Profile Picture is required")]
         public string ProfilePictureURL { get; set; }
 
         [Display(Name = "Full Name")]
+        [Required(ErrorMessage = "Full Name is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Full Name must be between 3 and 50 chars")]
         public string FullName { get; set; }
 
         [Display(Name = "Biography")]
+        [Required(ErrorMessage = "Biography is required")]
         public string Bio { get; set; }
 
-        // Relationships
-        public List<Actor_Movie> Actors_Movies { get; set; }
+        //Relationships
+        [BindNever] // added to fix the issue of invalid model 
+        public List<Actor_Movie>? Actors_Movies { get; set; }
 
     }
 }
